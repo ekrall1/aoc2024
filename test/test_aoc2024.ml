a0ecc3916e7506068d4b484de8f6b1a594983173
Eric Krall <krall.eric@gmail.com>
2024-12-01 21:58:40 -0500

    adding unit tests

---
+++ test/test_aoc2024.ml
@@ -0,0 +1,6 @@
+module Day1 = Days.D1
+
+(* day 1 tests *)
+let day1_test_input = "../../../test/d1_test"
+let () = assert (Day1.part1 day1_test_input = "11")
+let () = assert (Day1.part2 day1_test_input = "31")
